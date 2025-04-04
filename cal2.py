da2c9abbc43339f9ce3193cef9d92312cc275ed1
matthew <cmatthew.park@gmail.com>
2025-04-04 18:40:05 +0000

    s

--- cal2.py
+++ cal2.py
@@ -3,77 +3,85 @@ def add(a):
     sum = 0
     for i in a:
         sum += i
-    return sum
+    return "solution" + sum
 def sub(a):
     diff = a[0]-a[1]
-    return diff
+    return "solution" + diff
 def mult(a):
     prod = 1
     for i in a:
         prod *= i
-    return prod
+    return "solution" + prod
 def div(a):
     quot = a[0]/a[1]
-    return quot
+    return "solution" + quot
 def mod(a):
     mod = a[0] % a[1]
-    return mod
+    return "solution" + mod
 def floor(a):
     quot2 = a[0]//a[1]
-    return quot2
+    return "solution" + quot2
 
 def start():
-    #Hello welcome to the amazing calculator!
-    #Input any numbers along with operations (+,-,/,*,%,//)
+    print("Hello welcome to the amazing calculator!")
+    print("Input any numbers along with operations (+,-,/,*,%,//)")
     calc()
 def calc():
+    string = ""
+    inputs = []
     while True:
-        inputs = []
-        user_input = input("Input your calculation")
-        inputs.append[user_input]
-        for i in inputs:
-            string += i
-            print(string)
+        
+        
+        user_input = input("Input your calculation: ")
+        inputs.append(user_input)  
+        string += user_input
+        print(string)
         if user_input == "=":
             solution(inputs)
             break
 def solution(inputs):
-    num = [1,2,3,4,5,6,7,8,9,0]
+    
+    num = ['1','2','3','4','5','6','7','8','9','0']
     operator = ['+','-','/','*','%','//']
     ans = 0
-    while True:
+    index = -1
+    while True:    
+        for item in inputs:
+            if item in operator:
+                index = inputs.index(item)
+        if index == -1:
+            error()
+            break 
         
-        index = set(operator) & set(inputs)
-        if index == []:
+        if inputs[index-1] in num and inputs[index+1] in num:
+            insert = [int(inputs[index-1]),int(inputs[index+1])]
+            if inputs[index] == "+":
+                print(add(insert))
+                calc()
+            if inputs[index] == "-":
+                print(sub(insert))
+                calc()
+            if inputs[index] == "*":
+                print(mult(insert))
+                calc()
+            if inputs[index] == "/":
+                print(div(insert))
+                calc()
+            if inputs[index] == "%":
+                print(mod(insert))
+                calc()
+            if inputs[index] == "//":
+                print(floor(insert))
+                calc()
+        else:
             error()
             break
-        for i in index:
-            if input[i-1] in num and input[i+1] in num:
-                insert = [input[i-1],input[i+1]]
-                if input[i] == "+":
-                    ans += add(insert)
-                if input[i] == "-":
-                    ans += sub(insert)
-                if input[i] == "*":
-                    ans += mult(insert)
-                if input[i] == "/":
-                    ans += div(insert)
-                if input[i] == "%":
-                    ans += mod(insert)
-                if input[i] == "//":
-                    ans += floor(insert)
-
-                
-            
- 
-            else:
-                error()
-                break
 
 def error():
     print("error") 
     calc()
 
+start()
 
 
 
